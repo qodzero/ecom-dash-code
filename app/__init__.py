dd3a19033737fb5f563fc2db4b79f3dbb9ab6e13
Samuel Mthembo <fourkay537@gmail.com>
2022-03-22 15:11:38 +0200

    Part 02 - Add NavBar Tabs

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,29 @@
 
 from kivy.app import App
+from kivy.utils import QueryDict, rgba
+from kivy.metrics import dp, sp
 
 from .view import MainWindow
 
 class MainApp(App):
+    colors = QueryDict()
+    colors.primary = rgba("#2D9CDB")
+    colors.secondary = rgba("#16213E")
+    colors.success = rgba("#1FC98E")
+    colors.warning = rgba("#F2C94C")
+    colors.danger = rgba("#EB5757")
+
+    fonts = QueryDict()
+    fonts.size = QueryDict()
+    fonts.size.h1 = dp(24)
+    fonts.size.h2 = dp(22)
+    fonts.size.h3 = dp(18)
+    fonts.size.h4 = dp(16)
+    fonts.size.h5 = dp(14)
+    fonts.size.h6 = dp(12)
+
+    fonts.heading = 'assets/fonts/Roboto/Roboto-Bold.ttf'
+    fonts.subheading = 'assets/fonts/Roboto/Roboto-Regular.ttf'
+    fonts.body = 'assets/fonts/Roboto/Roboto-Light.ttf'
     def build(self):
         return MainWindow()
--- app/view.py
+++ app/view.py
@@ -1,6 +1,15 @@
 
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.behaviors import ToggleButtonBehavior
+from kivy.properties import StringProperty
 
 class MainWindow(BoxLayout):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+class NavTab(ToggleButtonBehavior, BoxLayout):
+    text = StringProperty("")
+    icon = StringProperty("")
+    icon_active = StringProperty("")
     def __init__(self, **kw):
         super().__init__(**kw)
